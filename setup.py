75a0d2ae8dbc3064d83cdb17029248d016e4fa25
Dominik Engel <dominik.engel@mail.de>
2023-07-24 11:02:41 +0200

    Update setup.py
    update dependencies in setup.py

--- setup.py
+++ setup.py
@@ -75,11 +75,11 @@ setuptools.setup(
         'build_ext': BuildExtension
     },
     install_requires=[
-        'torch==1.9.0',
-        'networkx==2.6.2',
+        'torch>=1.9.0',
+        'networkx>=2.6.2',
         'pytest==6.2.4',
         'loguru==0.5.3',
-        'scipy==1.7.1'
+        'scipy>=1.7.1'
     ],
     packages=find_packages(),
 )
